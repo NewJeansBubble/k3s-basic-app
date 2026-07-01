0e38c484513a25cc383f0d5473d0ee4f643799fb
NJZ <semrumoyt@gmail.com>
2026-07-01 15:13:36 -0300

    chore(backend): configure eslint

---
+++ backend/eslint.config.js
@@ -0,0 +1,28 @@
+import { defineConfig } from "eslint/config";
+import js from "@eslint/js";
+import globals from "globals";
+
+export default defineConfig([
+	{
+		files: ["**/*.js"],
+		plugins: { js },
+		extends: ["js/recommended"],
+
+		languageOptions: {
+			globals: globals.node,
+		},
+
+		rules: {
+			eqeqeq: ["error", "always"],
+			curly: ["error", "all"],
+			"no-var": "error",
+			"prefer-const": "error",
+			"no-unused-vars": [
+				"error",
+				{
+					argsIgnorePattern: "^_",
+				},
+			],
+		},
+	},
+]);

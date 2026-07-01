298774facdcbb20093727b6aaf35a04320685641
NJZ <semrumoyt@gmail.com>
2026-07-01 15:20:00 -0300

    chore(backend): configure prettier

--- backend/eslint.config.js
+++ backend/eslint.config.js
@@ -1,28 +1,28 @@
-import { defineConfig } from "eslint/config";
-import js from "@eslint/js";
-import globals from "globals";
+import { defineConfig } from 'eslint/config';
+import js from '@eslint/js';
+import globals from 'globals';
 
 export default defineConfig([
-	{
-		files: ["**/*.js"],
-		plugins: { js },
-		extends: ["js/recommended"],
+  {
+    files: ['**/*.js'],
+    plugins: { js },
+    extends: ['js/recommended'],
 
-		languageOptions: {
-			globals: globals.node,
-		},
+    languageOptions: {
+      globals: globals.node,
+    },
 
-		rules: {
-			eqeqeq: ["error", "always"],
-			curly: ["error", "all"],
-			"no-var": "error",
-			"prefer-const": "error",
-			"no-unused-vars": [
-				"error",
-				{
-					argsIgnorePattern: "^_",
-				},
-			],
-		},
-	},
+    rules: {
+      eqeqeq: ['error', 'always'],
+      curly: ['error', 'all'],
+      'no-var': 'error',
+      'prefer-const': 'error',
+      'no-unused-vars': [
+        'error',
+        {
+          argsIgnorePattern: '^_',
+        },
+      ],
+    },
+  },
 ]);

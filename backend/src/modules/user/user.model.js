5737646d9f0d1365f93d14f133dcf54fe7914f62
NJZ <semrumoyt@gmail.com>
2026-07-01 16:34:44 -0300

    fix(backend): fix schema typo

--- backend/src/modules/user/user.model.js
+++ backend/src/modules/user/user.model.js
@@ -2,8 +2,8 @@ import { integer, pgTable, varchar, timestamp } from 'drizzle-orm/pg-core';
 
 export const users = pgTable('users', {
   id: integer('id').primaryKey().generatedAlwaysAsIdentity(),
-  name: varchar('name', { lenght: 100 }).notNull(),
-  email: varchar('email', { lenght: 255 }).unique().notNull(),
+  name: varchar('name', { length: 100 }).notNull(),
+  email: varchar('email', { length: 255 }).unique().notNull(),
   passwordHash: varchar('password_hash').notNull(),
   createdAt: timestamp('created_at', { withTimezone: true }).defaultNow().notNull(),
 });

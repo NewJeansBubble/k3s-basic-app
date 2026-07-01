b57fbb04e187187f1118ccc19af55117adc31db3
NJZ <semrumoyt@gmail.com>
2026-07-01 15:42:59 -0300

    chore(backend): move app.js into src & install drizzle ORM

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,7 +3,7 @@ import express from 'express';
 const app = express();
 const port = process.env.PORT;
 
-app.use(express.json())
+app.use(express.json());
 
 app.listen(port, () => {
   console.log(`Hello World`);

35c9ff67ab630685c2a2c7be0260425bc536a9f7
NJZ <semrumoyt@gmail.com>
2026-07-02 00:46:15 -0300

    fix(backend): fix constructor typos

--- backend/src/shared/errors/app.error.js
+++ backend/src/shared/errors/app.error.js
@@ -2,11 +2,11 @@ export class AppError extends Error {
   constructor(message, statusCode, code, details = undefined) {
     super(message);
 
-    this.name = this.contructor.name;
+    this.name = this.constructor.name;
     this.statusCode = statusCode;
     this.code = code;
     this.details = details;
 
-    Error.captureStackTrace?.(this, this.contructor);
+    Error.captureStackTrace?.(this, this.constructor);
   }
 }

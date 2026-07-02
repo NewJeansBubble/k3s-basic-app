d7cc443bd3bdc14cf90abd2f97c1f543e0aae5f6
NJZ <semrumoyt@gmail.com>
2026-07-02 00:46:53 -0300

    feat(backend): create users custom exceptions

---
+++ backend/src/modules/user/user.errors.js
@@ -0,0 +1,13 @@
+import { AppError } from '../../shared/errors/app.error.js';
+
+export class UserEmailAlreadyExistsError extends AppError {
+  constructor() {
+    super('Email already in use', 409, 'USER_EMAIL_ALREADY_EXISTS');
+  }
+}
+
+export class UserNotFoundError extends AppError {
+  constructor() {
+    super('User not found', 404, 'USER_NOT_FOUND');
+  }
+}

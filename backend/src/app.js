50e06f6af60466c5f61935a39836d9cd73b51493
NJZ <semrumoyt@gmail.com>
2026-07-02 19:30:30 -0300

    feat(backend): add auth routes to the application

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import { userRoutes } from './modules/user/user.routes.js';
 import { errorHandler } from './shared/middleware/error.handler.js';
+import { authRoutes } from './modules/auth/auth.routes.js';
 
 const app = express();
 const port = process.env.PORT;
@@ -11,6 +12,7 @@ app.get('/', (_req, res) => {
   return res.json({ message: 'Hello World' });
 });
 
+app.use('/auth', authRoutes);
 app.use('/users', userRoutes);
 
 app.use(errorHandler);
